34c8004faa33e505f15f6e71544ca9e595d0ca0c
Henrik Grimler <henrik@grimler.se>
2021-03-10 14:49:38 +0100

    starship: enable http feature for CLI bug reporting
    See
    https://github.com/termux/termux-packages/pull/6501#discussion_r591495468

--- packages/starship/build.sh
+++ packages/starship/build.sh
@@ -7,7 +7,7 @@ TERMUX_PKG_SRCURL=https://github.com/starship/starship/archive/v$TERMUX_PKG_VERS
 TERMUX_PKG_SHA256=d8f4dc9bd266f2a5c34926d361c62fdddb61cd7da4acadba5f9c175eb07602e5
 TERMUX_PKG_DEPENDS="zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--no-default-features"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--no-default-features --features http"
 
 termux_step_pre_configure() {
 	CFLAGS+=" $CPPFLAGS"
